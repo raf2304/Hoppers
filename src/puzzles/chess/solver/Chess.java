c351412ab204f73f6fcee8c122eb4bc25d32a826
raf2304 <124389754+raf2304@users.noreply.github.com>
2023-09-17 20:40:04 -0400

    Delete src/puzzles/chess directory

--- src/puzzles/chess/solver/Chess.java
+++
@@ -1,9 +0,0 @@
-package puzzles.chess.solver;
-
-public class Chess {
-    public static void main(String[] args) {
-        if (args.length != 1) {
-            System.out.println("Usage: java Chess filename");
-        }
-    }
-}
